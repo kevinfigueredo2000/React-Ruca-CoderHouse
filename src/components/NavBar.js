073e685de7cdcbf4121542a55120e735f42fb1ad
kevinfigueredo <figueredo.kevin2000@gmail.com>
2021-12-08 13:01:28 -0300

    cambios

---
+++ src/components/NavBar.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function NavBar(){
+    return (
+        <nav>
+            <div>
+                <ul>
+                    <li>
+                        <a>Inicio</a>
+                    </li>
+                    <li>
+                        <a>Tienda</a>
+                    </li>
+                    <li>
+                        <a>Contacto</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
+export default NavBar;
