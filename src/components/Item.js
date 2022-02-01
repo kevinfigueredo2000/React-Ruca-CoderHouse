801717ef5e32650fafbaa75f5c307c8922314809
Kevin Figueredo <74889406+kevinfigueredo2000@users.noreply.github.com>
2022-02-01 19:39:39 -0300

    cambios

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,23 +1,23 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import ItemCount from "./ItemCount";
-import {Button} from 'react-bootstrap/';
+import {Container,Row, Button} from 'react-bootstrap/';
 import {useNavigate} from "react-router";
 
 function Item({product}){
     const navigate = useNavigate();
 
     return(
-        <>
-            <div className="card mt-3 mx-2 shadow-sm">
+        <Container>
+            <div className="card mt-3 mx-2 shadow-sm col">
                 <div className="card-body">
-                    <h5 className="card-title">{product.name}</h5>
-                    <p className="card-text">{product.tipodemate}</p>
-                    <img className="imagen-card img-thumbnail imag" src={product.name} alt={product.img}/>
-                    <Button className="mt-3 btn btn-secondary" onClick={()=>navigate(`/productos/${product.id}`)}>Ver detalle</Button>
+                    <h5 className="card-title text-center">{product.name}</h5>
+                    <p className="card-text text-center">{product.tipodemate}</p>
+                    <img className="imagen-card img-thumbnail imag text-center" src={product.name} alt={product.img}/>
+                    <Button className="mt-3 btn btn-secondary col-sm-6 text-center" onClick={()=>navigate(`/productos/${product.id}`)}>Ver detalle</Button>
                     <ItemCount stock={product.stock} initial={1}/>
                 </div>
             </div>
-        </>
+        </Container>
     );
 }
 
