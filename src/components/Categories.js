6554d99a92d0aeaa0b60cb36571332fb41fbc9bf
kevinfigueredo <figueredo.kevin2000@gmail.com>
2022-02-14 23:53:38 -0300

    cambios

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,50 +1,28 @@
 import "bootstrap/dist/css/bootstrap.css";
 import { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
-import { getFirestore } from "../firebase/index";
-import { Item } from "./Item/Item";
+import { Link } from "react-router-dom";
 
 const Categories=()=>{
-    const [data, setData] =useState([]);
+    const [products, setProducts] =useState();
     const [isLoading, setIsLoading] =useState(false)
-    const { categoryID } = useParams();
     useEffect(()=>{
+        const URL = `http://localhost:3001/productos`;
+        setIsLoading(true)
+        fetch(URL)
+        .then((res) => res.json())
+        .then((data) => setProducts(data))
+        .finally(()=> setIsLoading(false))
+    }, []);
+    if(isLoading || !products) return <p className="text-center" style={{fontSize:"30px"}}>Cargando...</p>
+    return(
         
-        const db = getFirestore() 
-        let productsCollection;
-        if(categoryID){
-           productsCollection = db.collection("productos")
-           .where("categoryID", "==", Number(categoryID))
-        }else{
-            productsCollection = db.collection("productos")
-        }
-       
-        const getDataFromFirestore = async ()=>{
-            setIsLoading(true);
-            try{
-                const response = await productsCollection.get();
-                if(response.empty) console.log("No hay productos");
-                setData(response.docs.map((doc)=> ({...doc.data(), id: doc.id})));
-            }finally{
-                setIsLoading(false)
-            }
-        }
-        
-        getDataFromFirestore();
-    }, [categoryID]);
-    if(isLoading){
-        return <p>Cargando los productos...</p>
-    }else{
-        return(
-            <div className="row">
-                {data.map((product)=> 
-                <div className="col-3">
-                    <Item key={product.key} product={product} />
-                </div>
-                )}
-            </div>
-        )
-    }
-    
+        <>
+            {products.map((product)=>(
+                    <div className="col-12 text-center" key={product.id}>
+                        <Link to="/" style={{textDecoration:"none"}}><p style={{fontSize:"30px", color:"black"}}>{product.category}</p></Link>
+                    </div>
+            ))}
+        </>
+    )
 }
 export default Categories;
