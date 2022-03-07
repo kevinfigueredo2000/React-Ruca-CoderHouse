c288ab241d30b88b4615ada7f313361f3d9821a9
kevinfigueredo <figueredo.kevin2000@gmail.com>
2022-03-07 00:10:35 -0300

    cambios

--- src/components/ThankYouPage/ThankYouPage.jsx
+++ src/components/ThankYouPage/ThankYouPage.jsx
@@ -20,6 +20,7 @@ const ThankYouPage =()=>{
     return (
         <div className="card text-center my-5" style={{justifyContent:"center", marginLeft:"auto", marginRight:"auto", width:"80%"}}>
             <h3>Detalle de su compra:</h3>
+                <h3 className="my-3 dataOperacion">Numero de compra: {order.id}</h3>
                 <h3 className="my-3">Total ${order.total}</h3>
             <Link to="/"><button className="btn btn-primary mt-3 mb-4" >Volver a la tienda</button></Link>
         </div>
