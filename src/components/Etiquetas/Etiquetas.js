8ccd1e6c29e198c947a3c595523db5a3b81ca59d
kevinfigueredo <figueredo.kevin2000@gmail.com>
2022-04-18 07:22:56 -0300

    responsive

--- src/components/Etiquetas/Etiquetas.js
+++ src/components/Etiquetas/Etiquetas.js
@@ -14,39 +14,13 @@ export const Etiquetas=()=>{
                             <div className="col-sm mt-4 mx-4">
                                 <h6 className="LGrisChico">Para compartir con amigos</h6>
                                 <h2>Hasta 12x sin interés</h2>
-                                <Link to="/categorias/2"><button className="btn btn-primary">Ver más</button></Link>
+                                <Link to="/categorias/2"><button className="btn btn-primary verMas1">Ver más</button></Link>
                             </div>
-                            <div className="col-sm col-12">
-                                <img className="imgsRandom" alt="imgsRandom" src="https://http2.mlstatic.com/termos-thermos-pack-2-liq-color-1lt-acero-inoxidable-surtido-D_NQ_NP_631127-MLC26776791371_022018-F.jpg"/>
-                            </div>
-                        </Row>
-                    </div>
-                    <div className="card col-sm mx-1 shadow">
-                        <Row className="my-3">
-                            <div className="col-sm mt-4 mx-4">
-                                <h6 className="LGrisChico">Para disfrutar solo</h6>
-                                <h2>Hasta 6x sin interés</h2>
-                                <Link to="/categorias/1"><button className="btn btn-primary">Ver más</button></Link>
-                            </div>
-                            <div className="col-sm col-12">
-                                <img className="imgsRandom" alt="imgsRandom" src='https://th.bing.com/th/id/OIP.GgeVtGBt8j-VYyfbQV07BwHaHa?pid=ImgDet&rs=1'/>
-                            </div>
-                        </Row>
-                    </div>
-                </Row>
-            </Container>
-            <Container id="descubri-cel">
-                <Row>
-                <div className="card col-sm mx-1 shadow">
-                        <Row className="my-3">
-                            <div className="col-sm mt-4 mx-4">
-                                <h6 className="LGrisChico">Para disfrutar solo</h6>
-                                <h2>Hasta 6x sin interés</h2>
-                               
-                            </div>
-                            <div className="col-sm col-12"> 
-                                <img className="imgsRandom" alt="imgsRandom" src='https://http2.mlstatic.com/termos-thermos-pack-2-liq-color-1lt-acero-inoxidable-surtido-D_NQ_NP_631127-MLC26776791371_022018-F.jpg'/>
-                                <Link to="/categorias/1"><button className="btn btn-primary verMas2">Ver más</button></Link>
+                            <div className="col-sm">
+                                <Row>
+                                    <img className="imgsRandom col-6" alt="imgsRandom" src="https://http2.mlstatic.com/termos-thermos-pack-2-liq-color-1lt-acero-inoxidable-surtido-D_NQ_NP_631127-MLC26776791371_022018-F.jpg"/>
+                                    <Link to="/categorias/1" className="col-4"><button className="btn btn-primary verMas2">Ver más</button></Link>
+                                </Row>
                             </div>
                         </Row>
                     </div>
@@ -55,10 +29,13 @@ export const Etiquetas=()=>{
                             <div className="col-sm mt-4 mx-4">
                                 <h6 className="LGrisChico">Para disfrutar solo</h6>
                                 <h2>Hasta 6x sin interés</h2>
+                                <Link to="/categorias/1"><button className="btn btn-primary verMas1">Ver más</button></Link>
                             </div>
-                            <div className="col-sm col-12">
-                                <img className="imgsRandom col-8" alt="imgsRandom" src='https://th.bing.com/th/id/OIP.GgeVtGBt8j-VYyfbQV07BwHaHa?pid=ImgDet&rs=1'/>
-                                <Link to="/categorias/1" className="col-4"><button className="btn btn-primary verMas2">Ver más</button></Link>
+                            <div className="col-sm">
+                                <Row>
+                                    <img className="imgsRandom col-6" alt="imgsRandom" src='https://th.bing.com/th/id/OIP.GgeVtGBt8j-VYyfbQV07BwHaHa?pid=ImgDet&rs=1'/>
+                                    <Link to="/categorias/1" className="col-4"><button className="btn btn-primary verMas2">Ver más</button></Link>
+                                </Row>
                             </div>
                         </Row>
                     </div>
