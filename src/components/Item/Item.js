bb2554c27c62ed4ba83f2632994e4b64cb45f25e
kevinfigueredo <figueredo.kevin2000@gmail.com>
2023-01-16 02:32:36 -0300

    cambios

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -5,10 +5,10 @@ import React from 'react';
 export const Item = ({ product }) => {
     let navigate = useNavigate();
     return (
-        <div className="card mt-3 shadow" style={{height:"32vh"}}>
+        <div className="card mt-3 shadow" style={{height:"32vh !important"}}>
             <div className="card-body text-center">
                 <img className="img-thumbnail" src={product.img} alt={product.name} style={{height:"17vh", width:"auto"}}/>
-                <h6 className="card-title mt-3">{product.name.length >= 25 ?  product.name.slice(0, 25) + "..." : product.name}</h6>
+                <h6 className="card-title mt-3">{product.name.length >= 25 ?  product.name.slice(0, 25) + "..." : product.name }</h6>
                 <button onClick={() => navigate(`/productos/${product.id}`)} className="btn btn-primary mb-3">Ver detalle</button>
             </div>
         </div>
