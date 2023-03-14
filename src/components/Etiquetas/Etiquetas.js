74b1d05b3c57b2e5db23c4e5d6f849a2fb54c1da
Kevin Figueredo <74889406+kevinfigueredo2000@users.noreply.github.com>
2023-03-14 17:39:06 -0300

    cambios

--- src/components/Etiquetas/Etiquetas.js
+++ src/components/Etiquetas/Etiquetas.js
@@ -1,15 +1,15 @@
 import { Row, Container } from "react-bootstrap";
 import "./Etiquetas.css"
 import { Link } from "react-router-dom";
-import React  from 'react';
+import React from 'react';
 import { useTiendaContext } from "../../context/TiendaContext";
 
-export const Etiquetas=()=>{
+export const Etiquetas = () => {
     const { categorias, setCategory } = useTiendaContext();
     const mate = 2;
     const termo = 0;
 
-    return(
+    return (
         <>
             <h3 className="LGris my-3">Descubrí</h3>
             <Container id="descubri-pc">
@@ -19,11 +19,11 @@ export const Etiquetas=()=>{
                             <div className="col-lg mt-lg-4 mx-lg-4">
                                 <h6 className="LGrisChico">Para compartir con amigos</h6>
                                 <h2>Hasta 12x sin interés</h2>
-                                <Link to="/categorias/2"><button className="btn btn-primary verMas1">Ver más</button></Link>
+                                <Link onClick={() => { setCategory(categorias[termo]?.name) }}><button className="btn btn-primary verMas1">Ver más</button></Link>
                             </div>
                             <div className="col-lg col-12">
                                 <Row>
-                                    <img className="col-sm-6 col-6" alt="imgsRandom" src="https://http2.mlstatic.com/termos-thermos-pack-2-liq-color-1lt-acero-inoxidable-surtido-D_NQ_NP_631127-MLC26776791371_022018-F.jpg"/>
+                                    <img className="col-sm-6 col-6" alt="imgsRandom" src="https://http2.mlstatic.com/termos-thermos-pack-2-liq-color-1lt-acero-inoxidable-surtido-D_NQ_NP_631127-MLC26776791371_022018-F.jpg" />
                                     <Link to={"/tienda/categorias/" + categorias[0]?.id} className="col-lg-3 col mt-auto" onClick={() => { setCategory(categorias[termo]?.name) }}><button className="btn btn-primary verMas2">Ver más</button></Link>
                                 </Row>
                             </div>
@@ -34,11 +34,11 @@ export const Etiquetas=()=>{
                             <div className="col-lg mt-lg-4 mx-lg-4">
                                 <h6 className="LGrisChico">Para disfrutar solo</h6>
                                 <h2>Hasta 6x sin interés</h2>
-                                <Link to="/categorias/1"><button className="btn btn-primary verMas1">Ver más</button></Link>
+                                <Link to={"/tienda/categorias/" + categorias[2]?.id} onClick={() => { setCategory(categorias[mate]?.name) }}><button className="btn btn-primary verMas1">Ver más</button></Link>
                             </div>
                             <div className="col-lg col-12">
                                 <Row>
-                                    <img className="col-lg-6 col-6" alt="imgsRandom" src='https://d3ugyf2ht6aenh.cloudfront.net/stores/806/999/products/de2a16161-241c2e1eb65d04468e16070467527067-1024-1024.jpg'/>
+                                    <img className="col-lg-6 col-6" alt="imgsRandom" src='https://d3ugyf2ht6aenh.cloudfront.net/stores/806/999/products/de2a16161-241c2e1eb65d04468e16070467527067-1024-1024.jpg' />
                                     <Link to={"/tienda/categorias/" + categorias[2]?.id} className="col-lg-3 col mt-auto" onClick={() => { setCategory(categorias[mate]?.name) }}><button className="btn btn-primary verMas2">Ver más</button></Link>
                                 </Row>
                             </div>
--- src/components/Slider/BtnSlider.js
+++ src/components/Slider/BtnSlider.js
@@ -1,15 +1,15 @@
 import React from "react";
 import "./Slider.css";
-import leftArrow from "./Icons/left-arrow.svg";
-import rightArrow from "./Icons/right-arrow.svg";
+import leftArrow from "./Icons/left-arrow.png";
+import rightArrow from "./Icons/right-arrow.png";
 
 export const BtnSlider=({ direction, moveSlide }) =>{
   return (
-    <button
+    <span
       onClick={moveSlide}
       className={direction === "next" ? "btn-slide next" : "btn-slide prev"}
     >
-      <img alt="" src={direction === "next" ? rightArrow : leftArrow} />
-    </button>
+      <img alt="" src={direction === "next" ? rightArrow : leftArrow} className="img-slider"/>
+    </span>
   );
 }
